83b944c529bbe2400aae873356a6572ddc912024
Balieiro <balieiro1396@gmail.com>
2021-01-26 15:28:28 -0300

    fixed the loop in the len of link and title lists

--- folha_mch.py
+++ folha_mch.py
@@ -15,7 +15,7 @@ def scrap_title():
     soup = new_soup()
     a = soup.find_all('a', {'class':"c-main-headline__url"}) + soup.find_all('a', {'class':"c-list-links__url"})
     Titles = []
-    for i in range(15):
+    for i in range(min(len(a), 15)):
         x = re.sub(r'<.*?>', '', str(a[i]))
         x = re.sub(r'\s+\s.*?\s+\s', '', x)
         Titles.append(x.strip())
@@ -26,6 +26,6 @@ def scrap_title_link():
     soup = new_soup()
     b = soup.find_all('a', {'class':"c-main-headline__url"}, href=True) + soup.find_all('a', {'class':"c-list-links__url"}, href=True)
     links = []
-    for i in range(15):
+    for i in range(min(len(b), 15)):
         links.append(b[i]['href'])
     return links
